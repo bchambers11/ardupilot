cf293f41e68f0d270f8e4b5fdafa5666d3d88673
Brian T Chambers <brian.chambers@uconn.edu>
2019-03-04 22:11:20 -0500

    same as last

--- ArduCopter/mode_autorotation.cpp
+++ ArduCopter/mode_autorotation.cpp
@@ -87,8 +87,8 @@ void Copter::ModeAutoRoto::run()
         //if the wait time has passsed cut the engine -> can make this random
         if(idle_count > WAIT_TIME)    //need define for WAIT_TIME
         {
-          //FIX THIS ASAP 
-          rc_write(AP_MOTORS_HELI_SINGLE_RSC, 0);     //_motors should be accessible from here(?)
+          //FIX THIS ASAP
+          //rc_write(AP_MOTORS_HELI_SINGLE_RSC, 0);     //_motors should be accessible from here(?)
         }
         idle_count ++;
 
